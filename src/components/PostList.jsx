bb6e19dd2ef651d22f7fb549e29c96433b8065fd
Podkolzin Danila <podkolzindanila@bk.ru>
2024-05-15 14:04:59 +0300

    last commit

---
+++ src/components/PostList.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import PostItem from './PostItem';
+import { TransitionGroup, CSSTransition } from 'react-transition-group'
+
+const PostList = ({ posts, title, remove }) => {
+
+    if (!posts.length) {
+        return (
+            <h1 style={{ textAlign: 'center' }}>
+                Постов нет!
+            </h1>
+        )
+    }
+    return (
+        <div>
+            <h1 style={{ textAlign: 'center' }}>
+                {title}
+            </h1>
+            <TransitionGroup>
+                {posts.map((post, index) =>
+                    <CSSTransition
+                        key={post.id}
+                        timeout={500}
+                        classNames='post'
+                    >
+                        <PostItem remove={remove} number={++index} post={post} key={post.id} />
+                    </CSSTransition>
+                )}
+            </TransitionGroup>
+        </div>
+    );
+};
+
+export default PostList;
---
+++ src/pages/NotFoundPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const NotFoundPage = () => {
+    return (
+        <div>
+            <h1 style={{color: 'red'}}> †Страница не существует† </h1> <br></br>
+        </div>
+    );
+};
+
+export default NotFoundPage;
